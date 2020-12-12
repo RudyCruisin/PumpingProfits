758d5666b00d5b680d12870d72bdea3d6bf44ea1
Rudy Saavedra <rudysa2@gmail.com>
2020-12-12 11:27:38 -0500

    first commit

---
+++ src/components/stocksearch.jsx
@@ -0,0 +1,27 @@
+const Stocksearch = () => {
+
+    const matchTicker = () => {
+
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
+    return (
+
+        <div>
+            {/* <img src={'/pump.png'} alt = {"pumping profits logo"}></img> */}
+            <input
+                type="text"
+                name="stockTicker"
+                value="Enter Your Ticker"
+                onChange={matchTicker}
+
+            />
+            <button onClick={handleSubmit}>Enter</button>
+        </div>
+    )
+}
+
+export default Stocksearch;
